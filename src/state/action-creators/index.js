7ca3fdb0ad19cf9f515f289b461aa357fd15eaa4
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-02-12 16:10:05 +0530

    Initial commit

---
+++ src/state/action-creators/index.js
@@ -0,0 +1,21 @@
+export const depositMoney=(amount)=>
+{
+    return (dispatch)=>
+    {
+        dispatch({
+            type:"deposit",
+            payLoad:amount
+        })
+    }
+}
+
+export const withdrawMoney=(amount)=>
+{
+    return (dispatch)=>
+    {
+        dispatch({
+            type:"withdraw",
+            payLoad:amount
+        })
+    }
+}
